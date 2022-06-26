0d6e81d6c8fe9048d1da1451a550647b888d66bd
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-06-27 00:59:52 +0700

    added initializer

--- adapter.py
+++ adapter.py
@@ -1,4 +1,5 @@
 # Import goes brrr
+from time import sleep
 from utils.log_neko import message_info
 from utils.file_utilities import *
 from utils.config_utilities import *
@@ -6,17 +7,26 @@ from tools import *
 from utils.validation import *
 import threading
 
+global sigma 
 def init_adapter():
+    message_info("Initializing adapter")
     if isFirstRun():
         t = threading.Thread(target = init_first_run_sequence())
         t.start()
+        sleep(3)
+        init_user_auth()
     t.join()
     init_on_start()
     try:
-        from sigma_ciphers_cryptograms import core
+        from sigma_ciphers_cryptograms import core as sigma_core
     except ModuleNotFoundError:
         message_warn("Sigma Ciphers Cryptograms is not installed")
         message_warn("Please wait, until it is installed")
+    global sigma #sigma_core.Sigma()
+    sigma = sigma_core.Sigma()
+    message_info("Initialization completed")
+
+    
         
 
         
