2533536aeb277420855060edfd6ab938aaf95fc9
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-06-26 21:10:51 +0700

    added tools for update and satisfy dependencies

---
+++ tools.py
@@ -0,0 +1,40 @@
+from asyncio import subprocess
+from utils.config_utilities import *
+import subprocess
+
+
+repos_link = load_config("config.json")
+
+def clone(repos):
+    try:
+        subprocess.Popen(['git', 'clone' , repos], shell=True)
+        print("succesfully cloned" , repos)
+    except Exception as e:
+        print(e)
+
+def pull(path):
+    try:
+        subprocess.Popen(['git', 'pull'], cwd=path, shell=True)
+        print("succesfully updated" , path)
+    except Exception as e:
+        print(e)
+    
+def satisfy_dependencies():
+    for repos in repos_link:
+        clone(repos)
+    print("succesfully cloned all required dependencies repos")
+
+def update():
+    try:
+        pull(load_config("config.json")["cipher_path"])
+        pull(load_config("config.json")["mcp_path"])
+        print("succesfully updated")
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    update()
+    #repos = "https://github.com/AkuraDiary/sigma-ciphers-cryptograms.git"
+    
+    #clone(repos)
+    #pull("./sigma-ciphers-cryptograms")
