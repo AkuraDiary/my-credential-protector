8cef7eb0593b17d6158540757dda847753c0e3a9
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-06-28 12:37:37 +0700

    testing simple ui mechanism

--- interfaces/ui.py
+++ interfaces/ui.py
@@ -1,7 +1,10 @@
 import sys
+from matplotlib.pyplot import text
+
+from pygame import init
 from utils.log_neko import *
 import os
-import tkinter as tk
+from tkinter import *
 # getting the name of the directory
 # where the this file is present.
 current = os.path.dirname(os.path.realpath(__file__))
@@ -19,12 +22,49 @@ sys.path.append(parent)
 import adapter
 from utils.config_utilities import *
 
+def fill(component, data):
+    component.config(state=NORMAL)
+    component.insert(data, text)
+    component.config(state=DISABLED)
 
 def init_ui():
-    window = tk.Tk()
+    """
+    COMPONNENTS
+    """
+    window = Tk()
+    text_box = Text(window, width=50, height=10, ).config(state=DISABLED)
+    
+    btn=Button(window, text="This is Button widget", fg='white')
+    btn.place(x=80, y=200)
+    """
+    COMPONNENTS
+    """
+    
+    """
+    PACK
+    """
+    btn.pack()
+    text_box.pack()
+    """
+    PACK
+    """
+
+    """
+    BINDING
+    """
+    btn.bind("<Button-1>", fill(text_box, "Hello World"))
+    """
+    BINDING
+    """
+
+    ## WINDOW
+    window.attributes("-topmost", True)
     window.title("MCP")
     window.geometry("500x500")
     window.mainloop()
+    ## WINDOW
+
+    
     return window
 
 def use_ui():
